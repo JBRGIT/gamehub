1577e301839468d3a6945bc62e126da004322d51
Jibril RADI <jradi@insy2s.fr>
2025-11-08 12:25:07 +0100

    Ajout du projet Spring Boot GameHub

---
+++ src/main/java/com/gamehub/gamehub_api/GamehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamehub.gamehub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamehubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamehub/gamehub_api/GamehubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamehub.gamehub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
