69d80cf1ba945af6693f80179e85e3c0703a5fc8
Jibril RADI <jradi@insy2s.fr>
2025-11-09 14:07:22 +0100

    4.2

--- src/main/java/com/gamehub/gamehub_api/mapper/GameMapper.java
+++ src/main/java/com/gamehub/gamehub_api/mapper/GameMapper.java
@@ -1,7 +1,24 @@
 package com.gamehub.gamehub_api.mapper;
 
+import com.gamehub.gamehub_api.dto.request.GameCreateRequest;
+import com.gamehub.gamehub_api.dto.request.GameUpdateRequest;
+import com.gamehub.gamehub_api.dto.response.GameDetailResponse;
+import com.gamehub.gamehub_api.dto.response.GameSummaryResponse;
+import com.gamehub.gamehub_api.entity.Game;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring")
 public interface GameMapper {
+    GameDetailResponse gameToGameDetailResponse(Game game);
+
+    GameSummaryResponse gameToGameSummaryResponse(Game game);
+
+    Game gameCreateRequestToGame(GameCreateRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
+    void gameUpdateRequestToGame(GameUpdateRequest request, @MappingTarget Game game);
 }
