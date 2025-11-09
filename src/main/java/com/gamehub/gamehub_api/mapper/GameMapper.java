1393ac437a0cd0cda74fac378d67532a7a0d2f9d
Jibril RADI <jradi@insy2s.fr>
2025-11-09 13:33:51 +0100

    4.1

---
+++ src/main/java/com/gamehub/gamehub_api/mapper/GameMapper.java
@@ -0,0 +1,7 @@
+package com.gamehub.gamehub_api.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GameMapper {
+}
---
+++ target/generated-sources/annotations/com/gamehub/gamehub_api/mapper/GameMapperImpl.java
@@ -0,0 +1,13 @@
+package com.gamehub.gamehub_api.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-09T13:33:18+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
+)
+@Component
+public class GameMapperImpl implements GameMapper {
+}
