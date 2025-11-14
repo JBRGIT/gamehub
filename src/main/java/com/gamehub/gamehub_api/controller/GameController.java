e50479cb2940dfae49bac74332066d64edd0956b
Jibril RADI <jradi@insy2s.fr>
2025-11-14 16:20:28 +0100

    7.2

---
+++ src/main/java/com/gamehub/gamehub_api/controller/GameController.java
@@ -0,0 +1,77 @@
+package com.gamehub.gamehub_api.controller;
+
+import com.gamehub.gamehub_api.dto.request.GameCreateRequest;
+import com.gamehub.gamehub_api.dto.request.GameUpdateRequest;
+import com.gamehub.gamehub_api.dto.response.GameDetailResponse;
+import com.gamehub.gamehub_api.dto.response.GameSummaryResponse;
+import com.gamehub.gamehub_api.entity.GameCategory;
+import com.gamehub.gamehub_api.service.GameService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
+@RequiredArgsConstructor
+@Slf4j
+public class GameController {
+    private final GameService gameService;
+
+    @PostMapping
+    public ResponseEntity<GameDetailResponse> createGame(@Valid @RequestBody GameCreateRequest gameCreateRequest) {
+        log.info("Requête POST reçue pour créer un jeu : {}", gameCreateRequest.getTitle());
+        GameDetailResponse gameDetailResponse = gameService.createGame(gameCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(gameDetailResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameDetailResponse> getGameById(@PathVariable Long id) {
+        log.info("Requête GET reçue pour récupérer le jeu ID : {}", id);
+        GameDetailResponse gameDetailResponse = gameService.getGameById(id);
+        return ResponseEntity.ok(gameDetailResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GameSummaryResponse>> getAllGames(@RequestParam(required = false) GameCategory category, @RequestParam(required = false, defaultValue = "false") Boolean availableOnly) {
+        log.info("Requête GET reçue pour récupérer les jeux : category={}, availableOnly={}", category, availableOnly);
+        List<GameSummaryResponse> list;
+        if (category != null) {
+            log.info("Filtrage par catégorie : {}", category);
+            list = gameService.getGamesByCategory(category);
+        } else if (availableOnly) {
+            log.info("Filtrage : uniquement les jeux disponibles");
+            list = gameService.getAvailableGames();
+        } else {
+            log.info("Aucun filtre appliqué : récupération de tous les jeux");
+            list = gameService.getAllGames();
+        }
+
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GameDetailResponse> updateGame(@PathVariable Long id, @Valid @RequestBody GameUpdateRequest request) {
+        log.info("Requête Put reçue pour mettre à jour le jeu ID : {}", id);
+        GameDetailResponse response = gameService.updateGame(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long id) {
+        log.info("Requête DELETE reçue pour supprimer le jeu ID : {}", id);
+        gameService.deleteGame(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/unavailable")
+    public ResponseEntity<Void> markAsUnavailable(@PathVariable Long id) {
+        log.info("Requête PATCH reçue pour marquer le jeu ID {} comme indisponible", id);
+        gameService.markAsUnavailable(id);
+        return ResponseEntity.noContent().build();
+    }
+}
